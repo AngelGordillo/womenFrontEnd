1ccb7f9d3ef173b02c6a373607a1de74b6a0ff86
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-21 16:29:39 +0200

    stage 1 done

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -10,9 +10,13 @@ var myApp = angular
       //Remove the header used to identify ajax call  that would prevent CORS from working
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
-.controller("womenController", ["$scope",'$filter',"$http", function($scope,$filter, $http){
+.controller("womenController", ["$scope",'$filter',"$http",'$window' ,function($scope,$filter, $http, $window){
 
 		var urlGet = "http://localhost:8080/women";
+		var urlUser = "http://localhost:8080/users";
+		var home = "http://localhost:8080/womenValidate/";
+		var urlWomenActivate = ""
+
 		$http({
 			method: 'GET',
 			url: urlGet,
@@ -26,11 +30,26 @@ var myApp = angular
 
 		$scope.sendData = function () {
 			
-			var Indata = {'name': $scope.name, 'topic': $scope.topic,'picture_url': $scope.picturerl };
+			var Indata = {'name': $scope.name, 'email': $scope.email,'picture_url': $scope.pictureUrl, 'topic': $scope.topic, 'linkedin': $scope.linkedin };
 			$http.post(urlGet, Indata).then(function (data, status, headers, config) { 
 				console.log(Indata);
 
 			    alert("success"); 
+			},function (data, status, headers, config) { 
+				console.log(Indata);
+			    alert("error"); 
+			});
+
+			}
+			$scope.sendDataUser = function () {
+			
+			var Indata = {'isPublic': true};
+			home += $scope.name;
+			console.log(home);
+			$http.put(home, Indata).then(function (data, status, headers, config) { 
+				console.log(Indata);
+			    alert("success"); 
+			    $window.location.href = home;
 			},function (data, status, headers, config) { 
 			    alert("error"); 
 			});
