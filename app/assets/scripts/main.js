40b9f635498ac3eaaa98e38f07c9ae26be133519
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-23 15:37:37 +0200

    test1

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -3,16 +3,16 @@ var config = require('./config.js');
 const angular = require('angular');
 var myApp = angular
 .module('myApp', [])
-.config(function($httpProvider) {
-      //Enable cross domain calls
-      $httpProvider.defaults.useXDomain = true;
+// .config(function($httpProvider) {
+//       //Enable cross domain calls
+//       $httpProvider.defaults.useXDomain = true;
 
-      //Remove the header used to identify ajax call  that would prevent CORS from working
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  })
+//       //Remove the header used to identify ajax call  that would prevent CORS from working
+//       delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//   })
 .controller("womenController", ["$scope",'$filter',"$http",'$window' ,function($scope,$filter, $http, $window){
 
-		var urlGet = "https://whispering-castle-40376.herokuapp.com/women";
+		var urlGet = "https://tranquil-lowlands-85919.herokuapp.com/women";
 		var urlUser = "http://localhost:8080/users";
 		var home = "http://localhost:8080/womenValidate/";
 		var urlWomenActivate = ""
