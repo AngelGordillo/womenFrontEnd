ba2676288b996329c281e6150f44e2e90c3cdbe7
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-30 11:10:06 +0200

    new version

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -9,5 +9,5 @@ module.exports = {
    urlPost : 'https://tranquil-lowlands-85919.herokuapp.com/women',
  urlGetFalse : 'https://tranquil-lowlands-85919.herokuapp.com/women?public=false',
  urlGetTrue : 'https://tranquil-lowlands-85919.herokuapp.com/women?public=true',
- urlUser : 'http://localhost:8080/users'
+ urlUser : 'https://tranquil-lowlands-85919.herokuapp.com/users'
 };
--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -8,11 +8,11 @@ var myApp = angular
 //       $httpProvider.defaults.useXDomain = true;
 
 //       //Remove the header used to identify ajax call  that would prevent CORS from working
-//       delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//       Activate $httpProvider.defaults.headers.common['X-Requested-With'];
 //   })
 .controller("womenController", ["$scope",'$filter',"$http",'$window' ,function($scope,$filter, $http, $window){
 	$scope.datas = [];
-
+	
 
 	$http({
 		method: 'GET',
@@ -55,10 +55,10 @@ var myApp = angular
 
 		var Indata = {'name': $scope.name, 'email': $scope.email,'picture_url': $scope.pictureUrl, 'topic': $scope.topic, 'linkedin': $scope.linkedin };
 		$http.post(config.urlPost, Indata).then(function (data, status, headers, config) { 
+			//$scope.idUser = $scope.datas.data.id;
 			console.log(Indata);
-
 			console.log("success"); 
-			$window.location.reload();
+			//$window.location.reload();
 		},function (data, status, headers, config) { 
 			console.log(Indata);
 			console.log("error"); 
@@ -76,6 +76,50 @@ var myApp = angular
 		},function (data, status, headers, config) { 
 
 			console.log(urlDeleteWoman);
+			console.log("error"); 
+		});
+	}
+		$scope.validateWomenTrue = function(){
+			var str = $window.location.href;
+			var parts = str.split('?');
+		var idUser =  parts[1];
+		var urlValidateWoman;		
+		urlValidateWoman = "https://tranquil-lowlands-85919.herokuapp.com/womenValidate/" + idUser + "?public=true";
+
+		console.log(idUser);
+		$http.get(urlValidateWoman).then(function (data, status, headers, config) { 	
+		$window.location.reload();	
+			console.log("success"); 
+		},function (data, status, headers, config) { 
+
+			console.log(urlActivateWoman);
+			console.log("error"); 
+		});
+	}
+			$scope.validateWomenFalse = function($event){
+		var urlDeleteWoman;		
+		urlDeleteWoman = "https://tranquil-lowlands-85919.herokuapp.com/womenDelete/" + idUser;
+
+		console.log(urlDeleteWoman);
+		$http.get(urlDeleteWoman).then(function (data, status, headers, config) { 	
+		$window.location.reload();	
+			console.log("success"); 
+		},function (data, status, headers, config) { 
+
+			console.log(urlDeleteWoman);
+			console.log("error"); 
+		});
+	}
+		$scope.sendDataUser = function($event){
+		var Indata = {'username' : $scope.username, 'password': $scope.password};
+		$http.post(config.urlUser, Indata).then(function (data, status, headers, config) { 	
+		var url = "http://" + $window.location.host + "/woman.html";
+		console.log($window.location.host)
+		$window.location.href = url	
+			console.log("success"); 
+		},function (data, status, headers, config) { 
+
+			
 			console.log("error"); 
 		});
 	}
